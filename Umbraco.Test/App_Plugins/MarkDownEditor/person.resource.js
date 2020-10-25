ddecd076833308ba358d47df18747c420e467b96
Tomas Foss Renaa <tomasrenaa@gmail.com>
2020-10-25 18:16:42 +0100

    fix

---
+++ Umbraco.Test/App_Plugins/MarkDownEditor/person.resource.js
@@ -0,0 +1,14 @@
+﻿// adds the resource to umbraco.resources module:
+angular.module('umbraco.resources').factory('personResource',
+    function ($q, $http, umbRequestHelper) {
+        // the factory object returned
+        return {
+            // this calls the ApiController we setup earlier
+            getAll: function () {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/My/PersonApi/GetAll"),
+                    "Failed to retrieve all Person data");
+            }
+        };
+    }
+);

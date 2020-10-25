3dcc667ad4bccde891a150c5352366231718bbd5
Tomas Foss Renaa <tomasrenaa@gmail.com>
2020-10-25 18:07:30 +0100

    rebuild

--- Umbraco.Test/App_Data/Models/all.generated.cs
+++ Umbraco.Test/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "862ae1584b2c7814")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs

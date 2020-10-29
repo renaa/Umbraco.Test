9bc6450da87d3c6f06e863efb7755f23ea98acd6
tomas <tomasrenaa@gmail.com>
2020-10-29 16:01:24 +0100

    example of a local surface controller

---
+++ Umbraco.Test/Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Umbraco.Test.Controllers
+{
+    public class MyController : Umbraco.Web.Mvc.SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return Content("Allo World");
+        }
+    }
+}
